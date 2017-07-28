cc21ea2a07be167c70e31196c7a270c3436f4e6a
samuelagyemang2012 <khermz2012@gmail.com>
2017-07-28 17:15:38 +0100

    k

--- www/js/myscript.js
+++ www/js/myscript.js
@@ -3,6 +3,7 @@
 //    alert(navigator.contacts);
 //}, true);
 
+var live_contacts = [];
 
 $(function () {
     $("[data-role=header]").toolbar();
@@ -126,8 +127,8 @@ function contacts_success(contacts) {
 
                     //$.cookie('i' + contacts[i].id, '' + contacts[id].id);
 
-                    build += "<div class='col-xs-12' id='" + contacts[i].id + "'onclick='select_contacts(" + contacts[i].id + ")'>";
-                    build += "<input style='opacity: 0' hidden value='false' id='i" + contacts[i].id + "'>";
+                    build += "<div class='col-xs-12' id='" + contacts[i].phoneNumbers[j].value + "'onclick='select_contacts(" + contacts[i].phoneNumbers[j].value + ")'>";
+                    build += "<input style='opacity: 0' hidden value='false' id='i" + contacts[i].phoneNumbers[j].value + "'>";
                     //build += "<input type='text' name='checkbox-1a' id='i" + contacts[i].id + "' checked=''>";
                     build += "<p><b style='color: #8E0D0E'>" + name + "</b></p>";
                     build += "<p><b style='color: #8E0D0E'>" + number + "</b></p>";
@@ -152,19 +153,36 @@ function to_contacts_page() {
     change_page("#contacts", "pop");
 }
 
-function select_contacts(id) {
+function select_contacts(num) {
 
-    var bool = $("#i" + id).val();
+    var bool = $("#i" + num).val();
 
     if (bool == "false") {
 
-        $("#" + id).css("background-color", "#EF9E9A");
-        $("#i" + id).val("true");
+        $("#" + num).css("background-color", "#EF9E9A");
+        $("#i" + num).val("true");
+        insert(num);
 
     } else {
-        //
-        $("#" + id).css("background-color", "#FFFFFF");
-        $("#i" + id).val("false");
+
+        $("#" + num).css("background-color", "#EEEEEE");
+        $("#i" + num).val("false");
+        del(num);
 
     }
+}
+
+function insert(data) {
+
+    if (live_contacts.indexOf(data) == -1) {
+        live_contacts.push(str1);
+    }
+}
+
+function del(data) {
+
+    var index = live_contacts.indexOf(data);
+
+    live_contacts.splice(index);
+
 }
